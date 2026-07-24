256c9635529aad2bcea60fc9fc7f6edbf8e44ea0
Liam Greenfield <52577696+Liam310@users.noreply.github.com>
2026-07-24 08:42:13 +0100

    fix array formatting

--- arrays/500.test.js
+++ arrays/500.test.js
@@ -15,8 +15,8 @@ function collectNumbers(list) {
 
 // NOTE: you do not need to change anything in the test
 test('only collects numbers in the array',() => {
-    const currentOutput = collectNumbers([10.1,"hello",6.1,8.0, 9.7, 10.1,"hi", 3.5,"oops"]);
-    const targetOutput = [10.1,6.1,8.0, 9.7, 10.1, 3.5,];
+    const currentOutput = collectNumbers([10.1, "hello", 6.1, 8.0, 9.7, 10.1, "hi", 3.5, "oops"]);
+    const targetOutput = [10.1, 6.1, 8.0, 9.7, 10.1, 3.5];
     
     expect(currentOutput).toEqual(targetOutput);
 });
